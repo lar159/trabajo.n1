a95f30339afee2bbfaa927a2c30497667cec1ef0
lar159 <luu.rivadeneira@gmail.com>
2023-05-18 14:54:14 -0300

    carga de los ejercicios al repositorio

---
+++ primerJS.js
@@ -0,0 +1,108 @@
+
+//document.write ("trabajo practico 1 JS ")
+ 
+//punto 1  Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+
+//alert ("Un mensaje")
+
+//punto 2 Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
+//document.write ("hello word")
+
+//punto 3  Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+//document.write (2 + 5)
+
+//punto 4 Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+
+//let nombre = prompt("Escribe tu nombre");
+//document.write("Hola "+ nombre);
+
+//punto 5  Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+
+
+//let num1 = parseInt(prompt ("escribe un numero"));
+//let num2 = parseInt(prompt ("escribe otro numero"));
+//let sumar = alert ( num1 + num2 );
+
+
+
+// //punto 6 escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+
+//let num1 = prompt("Escribe un número");
+// let num2 = prompt("Escribe otro número");
+// if (num1 > num2) {
+// document.write(num1);
+//  } else if { (num2 > num1)
+//  document.write(num2);
+//  } else {
+//  document.write("los numeros son iguales")
+//  }
+
+
+// //punto 7 Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+ //let num1 = parseInt(prompt("Escribe un numero:"))
+//  let num2 = parseInt(prompt("Escribe otro numero:"))
+//  let num3 = parseInt(prompt("Escribe otro numero:"))
+//  document.write("El numero mayor es " + Math.max(num1, num2, num3))
+
+
+// punto 8 Escribe un programa que pida un número y diga si es divisible por 2
+
+// let num1 = parseInt(prompt("Escribe un numero:"))
+
+// if (isNaN(num1)) {
+//     document.write("Tus padres son primos? Te dije que escribas un numero -.-")
+// } else if (num1 % 2 == 0) {
+//     document.write("El numero " + num1 + " es divisible por 2")
+// } else {
+//     document.write("El numero " + num1 + " no es divisible por 2")
+// }
+
+//punto 9 Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+// let texto = prompt("escribe una frase")   
+// let nTexto = texto.length  
+// for (i = 0; i < nTexto; i++)
+// if (texto.charAt(i) === "a" || texto.charAt(i) === "e" || texto.charAt(i) === "i" || texto.charAt(i) === "o" || texto.charAt(i) === "u")
+
+//punto 10  Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+
+// let numero1 = parseInt (prompt("ingrese un numero"))
+// if (numero1 % 2 == 0) { 
+//     document.write ("el numero $ {numero1} es divisible  en 2");
+// }
+// else if (numero1 % 3 == 0){
+//     document.write  ("el numero $ {numero1} es divisible en 3") 
+// } 
+// else if (numero1 % 5 == 0){
+//     document.write  ("el numero $ {numero1} es divisible en 5") 
+// } 
+// else if (numero1 % 7 == 0){
+//     document.write  ("el numero $ {numero1} es divisible en 7") 
+// } 
+// else {
+//     document.write  ("el numero $ {numero1} es divisible en 2 ni en 3 ni en 5 ni en 7");
+// } 
+
+
+// punto 11 Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+
+// let numero1 = parseInt (prompt(ingrese un numero))
+// if (numero1 % 2 == 0) {
+//     document.write ("el numero ${numero1} es divisible en 2");
+// }
+// if (numero1 % 3 == 0) {
+//     document.write ("el numero $ {numero1} es divisible en 3");
+// }
+// if (numero1 % 5 == 0) {
+//     document.write ("el numero ${numero1} es divisible en 5");
+// }
+// if (numero1 % 7 == 0) {
+//     document.write ("el numero $ {numero1} es divisible en 7");
+// }
+// if (numero1 % 2 !== 0 & numero1 % 3 !=0 & numero1 % 5 != 0 & numero1 % 7  != 0) {
+//     document.write ("el numero $ {numero1} no se puede dividir en 2, ni 3, 5 o 7");
+// }
+
